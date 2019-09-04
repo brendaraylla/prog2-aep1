94b4c702491c69aa869f2e74848b9d0d56395113
Unicesumar <brendaizui2@gmail.com>
2019-09-04 20:50:12 -0300

    first commit

---
+++ src/test/java/unicesumar/prog2/aep1/Aep1ApplicationTests.java
@@ -0,0 +1,16 @@
+package unicesumar.prog2.aep1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aep1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
